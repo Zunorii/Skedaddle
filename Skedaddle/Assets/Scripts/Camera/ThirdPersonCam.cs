edbaf871ccf38763fe2be59ca46bfb2652fec0db
McK-4 <117216683+McK-4@users.noreply.github.com>
2023-02-22 13:39:12 -0800

    Start of Third Person Cam/Movement
    Some minor fixes need to be added for the camera. The player can move, but is sort of teleporting for now.

---
+++ Skedaddle/Assets/Scripts/Camera/ThirdPersonCam.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCam : MonoBehaviour
+{
+    ThirdPersonMovement thirdPersonMoveScript;
+
+    public Transform orientation;
+    public Transform player;
+    public Transform playerObj;
+    public Rigidbody rb;
+
+    public float rotationSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 viewDir = player.position - new Vector3(transform.position.x, player.position.y, transform.position.z);
+        orientation.forward = viewDir.normalized;
+    }
+}
