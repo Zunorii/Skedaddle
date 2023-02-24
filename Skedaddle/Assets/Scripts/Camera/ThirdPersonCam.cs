666b1cec2002fc401727b9d1d643a0437310c4d4
McK-4 <117216683+McK-4@users.noreply.github.com>
2023-02-24 11:22:43 -0800

    Movement System Cont.

--- Skedaddle/Assets/Scripts/Camera/ThirdPersonCam.cs
+++ Skedaddle/Assets/Scripts/Camera/ThirdPersonCam.cs
@@ -16,12 +16,17 @@ public class ThirdPersonCam : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        thirdPersonMoveScript = playerObj.GetComponent<ThirdPersonMovement>();
     }
     // Update is called once per frame
     void Update()
     {
         Vector3 viewDir = player.position - new Vector3(transform.position.x, player.position.y, transform.position.z);
         orientation.forward = viewDir.normalized;
+
+        if(thirdPersonMoveScript.direction != Vector3.zero)
+        {
+            playerObj.forward = Vector3.Slerp(playerObj.forward, thirdPersonMoveScript.direction.normalized, Time.deltaTime * rotationSpeed);
+        }
     }
 }
--- Skedaddle/Assets/Scripts/Movement/ThirdPersonMovement.cs
+++ Skedaddle/Assets/Scripts/Movement/ThirdPersonMovement.cs
@@ -6,8 +6,22 @@ public class ThirdPersonMovement : MonoBehaviour
 {
     public CharacterController controller;
     public Transform cam;
+    public Rigidbody myRB;
+    public Transform orientation;
+
+    public float playerHeight;
+
+    public float horizontalInput;
+    public float verticalInput;
+
+    public Vector3 direction;
 
     public float speed = 6f;
+
+    //Ground
+    public LayerMask Ground;
+    public bool grounded;
+    public float groundDrag;
 
     public float turnSmoothTime = 0.1f;
     private float turnSmoothVelocity;
@@ -17,23 +31,53 @@ public class ThirdPersonMovement : MonoBehaviour
     {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+        myRB.freezeRotation = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
-        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
 
-        if (direction.magnitude >= 0.1f)
+        direction = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+        myRB.AddForce(direction.normalized * speed * 10f, ForceMode.Force);
+
+        //A raycast to make sure the player is on the ground
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, Ground);
+
+        if (grounded)
+            myRB.drag = groundDrag;
+        else
+            myRB.drag = 0;
+
+        Vector3 flatVel = new Vector3(myRB.velocity.x, 0f, myRB.velocity.z);
+        if (flatVel.magnitude > speed)
         {
-            float targetAngle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg + cam.eulerAngles.y;
-            float angle = Mathf.SmoothDamp(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-            transform.rotation = Quaternion.Euler(0f, angle, 0f);
-
-            Vector3 moveDir = Quaternion.Euler(0f, angle, 0f) * Vector3.forward;
-            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+            Vector3 limitedVel = flatVel.normalized * speed;
+            myRB.velocity = new Vector3(limitedVel.x, myRB.velocity.y, limitedVel.z);
         }
+
+        //Vector3 tempVelocity = myRB.velocity;
+        //tempVelocity.x = Input.GetAxisRaw("Horizontal") * speed;
+        //tempVelocity.y = Input.GetAxisRaw("Vertical") * speed;
+
+        //Vector3 velocity = (transform.forward * tempVelocity.y) + (transform.right * tempVelocity.x);
+        //velocity.y = myRB.velocity.y;
+        //myRB.velocity = velocity;
+        //    float horizontal = Input.GetAxisRaw("Horizontal");
+        //    float vertical = Input.GetAxisRaw("Vertical");
+        //    Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        //    if (direction.magnitude >= 0.1f)
+        //    {
+        //        float targetAngle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg + cam.eulerAngles.y;
+        //        float angle = Mathf.SmoothDamp(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+        //        transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+        //        Vector3 moveDir = Quaternion.Euler(0f, angle, 0f) * Vector3.forward;
+        //        controller.Move(moveDir.normalized * speed * Time.deltaTime);
+        //    }
     }
 }
